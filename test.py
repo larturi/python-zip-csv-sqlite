c017ac52d050e2249b016d68488fadcd48ab42ee
Leandro Arturi <lea.arturi@gmail.com>
2022-02-27 12:57:08 -0300

    Optimizaciones

--- test.py
+++ test.py
@@ -4,27 +4,25 @@ from coches import *
 nombre_fichero = 'test.csv'
 datos = leer_datos(nombre_fichero)
 
-    
+def set_environment():
+    borrar_bd()
+    conexion = crear_conexion_bd()
+    crear_tabla_coches(conexion)
+    grabar_coche(conexion, datos)
+    return conexion
 
 class test_count_coches(unittest.TestCase):
     def test_count_coches(self):
                 
-        borrar_bd()
-        
-        conexion = crear_conexion_bd()
-        crear_tabla_coches(conexion)
-        grabar_coche(conexion, datos)
+        conexion = set_environment()
+       
         count = count_coches(conexion)
-        
         self.assertEqual(count, 2780)
         
 class test_precio_total_coches(unittest.TestCase):
     def test_precio_total_coches(self):
         
-        borrar_bd()
-        conexion = crear_conexion_bd()
-        crear_tabla_coches(conexion)
-        grabar_coche(conexion, datos)
+        conexion = set_environment()
         
         suma = suma_importe_coches(conexion)
         self.assertEqual(suma, 38997136.0)
