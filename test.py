45d7b98fd426050907b5457179eccbcc624977b1
Leandro Arturi <lea.arturi@gmail.com>
2022-02-27 12:54:13 -0300

    Test suma importes coches

--- test.py
+++ test.py
@@ -2,19 +2,32 @@ import unittest
 from coches import *
 
 nombre_fichero = 'test.csv'
+datos = leer_datos(nombre_fichero)
+
+    
 
 class test_count_coches(unittest.TestCase):
     def test_count_coches(self):
+                
+        borrar_bd()
+        
+        conexion = crear_conexion_bd()
+        crear_tabla_coches(conexion)
+        grabar_coche(conexion, datos)
+        count = count_coches(conexion)
+        
+        self.assertEqual(count, 2780)
+        
+class test_precio_total_coches(unittest.TestCase):
+    def test_precio_total_coches(self):
         
         borrar_bd()
-        datos = leer_datos(nombre_fichero)
         conexion = crear_conexion_bd()
         crear_tabla_coches(conexion)
         grabar_coche(conexion, datos)
         
-        count = count_coches(conexion)
-
-        self.assertEqual(count, 2780)
+        suma = suma_importe_coches(conexion)
+        self.assertEqual(suma, 38997136.0)
         
         
 if __name__ == '__main__':
